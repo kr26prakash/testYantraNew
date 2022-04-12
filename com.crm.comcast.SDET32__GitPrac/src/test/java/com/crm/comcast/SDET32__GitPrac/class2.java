fce8ce5d2a2b96a06395f81799fb552844b1dd28
WIN8.1 <WIN8.1@PRAKASH.KR>
2022-04-12 12:33:53 +0530

    commit_to_change

--- com.crm.comcast.SDET32__GitPrac/src/test/java/com/crm/comcast/SDET32__GitPrac/class2.java
+++ com.crm.comcast.SDET32__GitPrac/src/test/java/com/crm/comcast/SDET32__GitPrac/class2.java
@@ -1,5 +1,14 @@
 package com.crm.comcast.SDET32__GitPrac;
 
+import org.testng.annotations.Test;
+
 public class class2 {
 
+	@Test
+	public void test3() {
+		System.out.println("chamge in class2");
+	}
+	
+	
+	
 }
