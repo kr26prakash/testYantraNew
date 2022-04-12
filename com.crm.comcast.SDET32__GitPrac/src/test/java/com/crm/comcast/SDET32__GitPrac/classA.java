61d4341e07b4c4ac32b738aaf6b89a96ce631a0b
WIN8.1 <WIN8.1@PRAKASH.KR>
2022-04-12 11:37:25 +0530

    commit1

---
+++ com.crm.comcast.SDET32__GitPrac/src/test/java/com/crm/comcast/SDET32__GitPrac/classA.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.SDET32__GitPrac;
+
+import org.testng.annotations.Test;
+
+public class classA {
+	@Test
+	public void prac() {
+		System.out.println("pushed to git");
+	}
+
+}
