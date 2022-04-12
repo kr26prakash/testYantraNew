4a7dbe5b2f29104a78ad23f56c44f46c67c38f1c
WIN8.1 <WIN8.1@PRAKASH.KR>
2022-04-12 12:48:38 +0530

    commit_update_NewChange

---
+++ com.crm.comcast.SDET32__GitPrac/src/test/java/com/crm/comcast/SDET32__GitPrac/class3.java
@@ -0,0 +1,12 @@
+package com.crm.comcast.SDET32__GitPrac;
+
+import org.testng.annotations.Test;
+
+public class class3 {
+
+	@Test
+	
+	public void test77() {
+		System.out.println("Creating new class");
+	}
+}
